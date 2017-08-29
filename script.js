87aaf6d187de09fafb0cc301a99c3a1e7a672c06
Zack Hartmann <zackhartmann@gmail.com>
2017-08-29 12:07:07 -0400

    Changed colors ordering schema

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var simonSays = {
 	sequence: [1,2,3,4,2,4,1,3],
 	boxColors:
 	{
-		1: { 0: '#003300', 1: '#00cc00'	}, 2: { 0: '#800000', 1: '#cc0000' },
-		3: { 0: '#000066', 1: '#0000ff' }, 4: { 0: '#663300', 1: '#cc9900' }
+		0: { 1: '#003300', 2: '#800000', 3: '#000066', 4: '#663300'},
+		1: { 1: '#00cc00', 2: '#cc0000', 3: '#0000ff', 4: '#cc9900'}
 	},
 	growSequence: function(addThisMany) {
 		while (--addThisMany > 0) {
@@ -17,9 +17,9 @@ var simonSays = {
 	},
 	illumeBox: function(box) {
 		setTimeout(function(){
-			document.getElementById(box).style.backgroundColor = simonSays.boxColors[box][1];
+			document.getElementById(box).style.backgroundColor = simonSays.boxColors[1][box];
 		}, 750);
-		document.getElementById(box).style.backgroundColor = simonSays.boxColors[box][0];
+		document.getElementById(box).style.backgroundColor = simonSays.boxColors[0][box];
 	}
 };
 
