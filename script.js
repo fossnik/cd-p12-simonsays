170efad65e630566dbf74ce52a0735bf80e0e8db
Zack Hartmann <zackhartmann@gmail.com>
2017-08-29 13:06:25 -0400

    Staggered Timeouts - Finally!

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var simonSays = {
-	sequence: [1,2,3,4,2,4,1,3],
+	sequence: [1,2,3,4],
 	boxColors:
 	{
-		0: { 1: '#003300', 2: '#800000', 3: '#000066', 4: '#663300'},
-		1: { 1: '#00cc00', 2: '#cc0000', 3: '#0000ff', 4: '#cc9900'}
+		0: { 1: '#003300', 2: '#660000', 3: '#000066', 4: '#663300'},
+		1: { 1: '#00cc00', 2: '#cc0000', 3: '#0040ff', 4: '#cc9900'}
 	},
 	growSequence: function(addThisMany) {
 		while (--addThisMany > 0) {
@@ -11,14 +11,15 @@ var simonSays = {
 		}
 	},
 	playSequence: function() {
-		this.sequence.forEach(function(box){
-			simonSays.illumeBox(box);
+		// uses the index to set the timeout
+		this.sequence.forEach(function(box, timeout){
+			simonSays.illumeBox(box, timeout);
 		});
 	},
-	illumeBox: function(box) {
+	illumeBox: function(box, timeout) {
 		setTimeout(function(){
 			document.getElementById(box).style.backgroundColor = simonSays.boxColors[1][box];
-		}, 750);
+		}, timeout * 1000);
 		document.getElementById(box).style.backgroundColor = simonSays.boxColors[0][box];
 	}
 };
