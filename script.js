4963234232536cd7ba03fdd7f135046f908316a2
Zack Hartmann <zackhartmann@gmail.com>
2017-08-31 12:13:48 -0400

    Needn't Add more than 1

--- script.js
+++ script.js
@@ -5,10 +5,8 @@ var simonSays = {
 		0: { 1: '#003300', 2: '#660000', 3: '#000066', 4: '#663300'},
 		1: { 1: '#00cc00', 2: '#cc0000', 3: '#0040ff', 4: '#cc9900'}
 	},
-	growSequence: function(addThisMany) {
-		while (--addThisMany > 0) {
-			this.sequence.push(Math.floor(Math.random()*4));
-		}
+	growSequence: function() {
+		this.sequence.push(Math.floor(Math.random()*4));
 	},
 	playSequence: function() {
 		// uses the index to set the timeout
