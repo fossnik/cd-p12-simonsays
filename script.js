acc7ebf72d2589cf2d7b8099a243261f2453c476
Zack Hartmann <zackhartmann@gmail.com>
2017-09-22 16:19:55 -0400

    audios working

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
+// beeps
+var beep1 = document.createElement('AUDIO');
+var src1 = document.createElement('SOURCE');
+src1.type= 'audio/mpeg';
+src1.src= "audio/1.mp3";
+beep1.appendChild(src1);
+var beep2 = document.createElement('AUDIO');
+var src2 = document.createElement('SOURCE');
+src2.type= 'audio/mpeg';
+src2.src= "audio/2.mp3";
+beep2.appendChild(src2);
+var beep3 = document.createElement('AUDIO');
+var src3 = document.createElement('SOURCE');
+src3.type= 'audio/mpeg';
+src3.src= "audio/3.mp3";
+beep3.appendChild(src3);
+var beep4 = document.createElement('AUDIO');
+var src4 = document.createElement('SOURCE');
+src4.type= 'audio/mpeg';
+src4.src= "audio/4.mp3";
+beep4.appendChild(src4);
+
 document.addEventListener("DOMContentLoaded", function(event) {
-	// simonSays.illumeBox(['a','d','b','c']);
 	simonSays.sequence = ['a','d','b','c'];
-
-	// make audio nodes
-	var beep1 = document.createElement('AUDIO');
-	var src1 = document.createElement('SOURCE');
-	src1.type= 'audio/mpeg';
-	src1.src= "audio/1.mp3";
-	beep1.appendChild(src1);
-	var beep2 = document.createElement('AUDIO');
-	var src2 = document.createElement('SOURCE');
-	src2.type= 'audio/mpeg';
-	src2.src= "audio/2.mp3";
-	beep2.appendChild(src2);
-	var beep3 = document.createElement('AUDIO');
-	var src3 = document.createElement('SOURCE');
-	src3.type= 'audio/mpeg';
-	src3.src= "audio/3.mp3";
-	beep3.appendChild(src3);
-	var beep4 = document.createElement('AUDIO');
-	var src4 = document.createElement('SOURCE');
-	src4.type= 'audio/mpeg';
-	src4.src= "audio/4.mp3";
-	beep4.appendChild(src4);
-
-	beep2.play();
 
 	simonSays.playSequence(simonSays.sequence)
   // setTimeout(function(){
@@ -51,6 +48,20 @@ var simonSays = {
 		boxes.forEach(function(box, timeout) {
 			setTimeout(function(){
 				document.getElementById(box).style.backgroundColor = simonSays.boxColors[1][box];
+				switch(box) {
+					case 'a':
+					beep1.play();
+					break;
+					case 'b':
+					beep2.play();
+					break;
+					case 'c':
+					beep3.play();
+					break;
+					case 'd':
+					beep4.play();
+					break;
+				}
 			}, timeout * 800);
 			setTimeout(function(){
 				document.getElementById(box).style.backgroundColor = simonSays.boxColors[0][box];
