cf5839a8acf1e99152636dd02592025901ad54a0
Zack Hartmann <zackhartmann@gmail.com>
2017-09-22 18:12:57 -0400

    edits

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 var beep1 = document.createElement('AUDIO');
-var src1 = document.createElement('SOURCE');
-src1.type= 'audio/mpeg';
-src1.src= "audio/1.mp3";
-beep1.appendChild(src1);
 var beep2 = document.createElement('AUDIO');
-var src2 = document.createElement('SOURCE');
-src2.type= 'audio/mpeg';
-src2.src= "audio/2.mp3";
-beep2.appendChild(src2);
 var beep3 = document.createElement('AUDIO');
-var src3 = document.createElement('SOURCE');
-src3.type= 'audio/mpeg';
-src3.src= "audio/3.mp3";
-beep3.appendChild(src3);
 var beep4 = document.createElement('AUDIO');
+var src1 = document.createElement('SOURCE');
+var src2 = document.createElement('SOURCE');
+var src3 = document.createElement('SOURCE');
 var src4 = document.createElement('SOURCE');
-src4.type= 'audio/mpeg';
-src4.src= "audio/4.mp3";
+src1.type = 'audio/mpeg';
+src2.type = 'audio/mpeg';
+src3.type = 'audio/mpeg';
+src4.type = 'audio/mpeg';
+src1.src = 'audio/1.mp3';
+src2.src = 'audio/2.mp3';
+src3.src = 'audio/3.mp3';
+src4.src = 'audio/4.mp3';
+beep1.appendChild(src1);
+beep2.appendChild(src2);
+beep3.appendChild(src3);
 beep4.appendChild(src4);
 
 document.addEventListener("DOMContentLoaded", function(event) {
 	simonSays.growSequence();
 	simonSays.playSequence(simonSays.sequence);
 });
+
+var handlers = {
+	tap: function(box) {
+		simonSays.illumeBox(box);
+	}
+};
 
 var simonSays = {
 	sequence: [],
@@ -44,28 +50,18 @@ var simonSays = {
 				document.getElementById(box).style.backgroundColor = simonSays.boxColors[1][box];
 				switch(box) {
 					case 'a':
-						beep1.play();
-						break;
+						beep1.play(); break;
 					case 'b':
-						beep2.play();
-						break;
+						beep2.play(); break;
 					case 'c':
-						beep3.play();
-						break;
+						beep3.play();	break;
 					case 'd':
-						beep4.play();
-						break;
+						beep4.play();	break;
 				}
 			}, timeout * 800);
 			setTimeout(function(){
 				document.getElementById(box).style.backgroundColor = simonSays.boxColors[0][box];
 			}, timeout * 800 + 300);
 		});
-	}
-};
-
-var handlers = {
-	tap: function(box) {
-		simonSays.illumeBox(box);
 	}
 };
